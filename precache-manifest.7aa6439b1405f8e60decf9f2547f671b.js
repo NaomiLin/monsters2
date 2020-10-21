d650ad319bc1190ff3f8f38bd9d1cc2031f37f42
NaomiLin <xiamiaolin@gmail.com>
2020-10-20 18:38:33 -0700

    Updates

---
+++ precache-manifest.7aa6439b1405f8e60decf9f2547f671b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c924916151efd9a51d7c48e88080abe6",
+    "url": "/monsters2/index.html"
+  },
+  {
+    "revision": "7247ac150f9c59d694b1",
+    "url": "/monsters2/static/css/main.5463dc6f.chunk.css"
+  },
+  {
+    "revision": "8c267033bfd0297d4089",
+    "url": "/monsters2/static/js/2.0e5329aa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters2/static/js/2.0e5329aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7247ac150f9c59d694b1",
+    "url": "/monsters2/static/js/main.191f63d1.chunk.js"
+  },
+  {
+    "revision": "7a832a24c01ee860a54e",
+    "url": "/monsters2/static/js/runtime-main.b17775c0.js"
+  }
+]);
